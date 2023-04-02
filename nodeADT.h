05b18e48193550e9e7fdded825480289963ddeb8
Yousefjoo17 <91394292+Yousefjoo17@users.noreply.github.com>
2023-04-02 22:36:00 +0200

    Update nodeADT.h

--- nodeADT.h
+++ nodeADT.h
@@ -8,8 +8,8 @@ public:
     
     virtual void setitem(const T& num)=0;
     virtual void setnext( node* nextptrpoiner)=0;
-    virtual T getitem()const;
-    virtual node* getnext()const;
+    virtual T getitem()const=0;
+    virtual node* getnext()const=0;
     
     
 };
